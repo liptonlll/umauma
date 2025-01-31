f8d67936820e9f8f7c9824510dfb740bb624c985
liptonlll <dex.fairy.zyx@gmail.com>
2025-02-01 01:31:02 +0900

    Delete React-Hook-Roulette-main directory

--- React-Hook-Roulette-main/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Roulette } from "./components/roulette";
-import { useRoulette } from "./hooks/use-roulette";
-import type { RouletteItem } from "./types";
-
-export { useRoulette, Roulette, RouletteItem };
